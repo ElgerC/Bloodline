1d6409de4d792c0029deeca76f548157513d147d
ElgerC <elger.creutzberg@gmail.com>
2025-03-27 12:18:05 +0100

    prototype guarding enemy

--- Assets/Scirpts/NPCS/GuardingEnemy.cs
+++ Assets/Scirpts/NPCS/GuardingEnemy.cs
@@ -4,5 +4,31 @@ using UnityEngine;
 
 public class GuardingEnemy : GuardScript
 {
-   
+    [Header("Guarding")]
+    [SerializeField] private Vector3 guardingPosition;
+    [SerializeField] private Vector3 guardingDirection;
+    
+    protected override void Start()
+    {
+        base.Start();
+
+        guardingDirection = transform.localRotation.eulerAngles;
+        guardingPosition = transform.localPosition;
+    }
+
+    protected override void BaseBehaviour()
+    {
+        agent.SetDestination(guardingPosition);
+        if (Vector3.Distance(transform.localPosition, guardingPosition) < 0.5)
+        {
+            agent.ResetPath();
+            transform.rotation = Quaternion.Euler(guardingDirection);
+        }
+    }
+
+    protected override void EndPatrol()
+    {
+        base.EndPatrol();
+        agent.SetDestination(guardingPosition);
+    }
 }
